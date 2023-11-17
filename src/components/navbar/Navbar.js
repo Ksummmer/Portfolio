bae8c377e06632b91dc9be070d0e6949955e183a
Kseniia Belova <kseniia_belova@web.de>
2023-11-17 13:47:25 +0100

    Update navbar txt

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
             <div className="container">
                 <div className="nav-row">
                     <NavLink to="/" className="logo">
-                        <strong>FullStack JavaScript Developer</strong> portfolio
+                        <strong>FullStack JavaScript Developer</strong>  <br />portfolio
                     </NavLink>
 
                     <ul className="nav-list">
